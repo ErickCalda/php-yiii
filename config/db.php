6edbd780a2b7c7e6146dfffddba99422c5fce7e8
ErickCalda <patriciocalda17@gmail.com>
2025-04-21 21:47:48 -0500

    1.0

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;port=3307;dbname=HBO',
+    'username' => 'estu_CALDAERICK',
+    'password' => 'c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+     //clave 1234567890 se aplico sha256 
+];
